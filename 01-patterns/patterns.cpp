0f6fce1f5191df2489831efdeb649f016cd28103
nitheeshmk41 <nitheeshc@protonmail.com>
2025-08-01 20:24:02 +0530

    coded till pattern07

--- 01-patterns/patterns.cpp
+++ 01-patterns/patterns.cpp
@@ -9,7 +9,76 @@ void pattern01(int n){
         cout << endl;
     }
 }
+void pattern02(int n){
+    for(int i = 0;i < n; i++){
+        for(int j = 0; j <= i;j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern03(int n){
+    for(int i = 0;i < n; i++){
+        for(int j = 0; j < n - i;j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+// NOTE : Consider only left space.
+void pattern04(int n){
+    for (int i = 1; i <= n; i++) {
+        // space
+        for (int j = 1; j <= n - i; j++) {
+            cout << "1";
+        }
+        // point
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern05(int n){
+    cout << "in\n";
+    for (int i = n; i >= 1; i--) {
+        //cout << "in";
+        // space
+        for (int space = 1; space <= n-i; space++) {
+            cout << " ";
+        }
+        //cout << "in";
+        // point
+        for (int j = 1; j <= 2*i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern06(int n){
+    for(int i = 0;i < n; i++){
+        for(int j = 0; j <= i;j++){
+            cout << j+1;
+        }
+        cout << endl;
+    }
+}
+
+//Floyd's Triangle
+void pattern07(int n){
+    int counter =1;
+    for(int i = 1;i <= n; i++){
+        for(int j = 1; j <= i;j++){
+            cout << counter++ << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
-    pattern01(5);
+    pattern07(7);
 }
