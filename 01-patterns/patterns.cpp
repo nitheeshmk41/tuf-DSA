75ae36a9281bf47fd5eeb897f28d5ef5c565d791
nitheeshmk41 <nitheeshc@protonmail.com>
2025-08-02 18:14:42 +0530

    coded till pattern10

--- 01-patterns/patterns.cpp
+++ 01-patterns/patterns.cpp
@@ -79,6 +79,34 @@ void pattern07(int n){
     }
 }
 
+
+void pattern10(int n){
+    for (int i = 0; i < n; i++)
+    {
+        if(i == 0 || i == n-1){
+            for(int j = 0; j < n;j++){
+                cout << "* ";
+            }
+        }else{
+            for(int k = 0; k < n ; k++){
+                if(k == 0 || k == n-1){
+                    cout << "* ";
+                }
+                else{
+                    cout <<"  ";
+                }
+            }
+        }
+        cout << endl;
+    }
+    
+}
+
+// Diamond
+void pattern11(int n){
+
+}
+
 int main(){
-    pattern07(7);
+    pattern10(7);
 }
